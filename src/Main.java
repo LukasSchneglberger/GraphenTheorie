b0c4922524993014ff6f6665b1c807d743271265
ZahrhuberV <valentin.zahrhuber@gmx.at>
2023-05-16 08:19:48 +0200

    created Graph Object

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.io.File;
+
 /**
  * @author Valentin Zahrhuber, Lukas Schneglberger
  */
 public class Main {
     
     public static void main(String[] args) {
-        
+        Graph graph = new Graph();
+        graph.read(new File(""));
     }
     
 }
